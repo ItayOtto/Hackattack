fe03c4f9d659a8d2aab742c7e0c27fe0a9404f78
Itay Ottolenghi <trombi1000@gmail.com>
2023-06-21 22:54:13 +0300

    trying

--- main.py
+++ main.py
@@ -4,43 +4,64 @@ from PIL import ImageGrab
 from enum import Enum
 
 
+width = 16#18
+height = 30#20
+
+
+# Number Color
+emtpy = (156, 81, 8)
+flag = (250, 253, 255)
+gray0 = (76, 76, 76)
+sky1 = (8, 195, 255)
+green2 = (110, 218, 9)
+red3 = (209, 64, 64)
+purple4 = (176, 90, 224)
+orange5 = (236, 145, 11)
+blue6 = (101, 140, 235)
+# TODO:
+color7 = (101, 140, 235)
+color8 = (101, 140, 235)
+
+
 class Tiles(Enum):
-    EMPTY = 0
-    FLAG = 1
-    GRAY0 = 2
-    SKY1 = 3
-    GREEN2 = 4
-    RED3 = 5
-    PURPLE4 = 6
-    ORANGE5 = 7
-    BLUE6 = 8
+    EMPTY = 9
+    FLAG = -1
+    GRAY0 = 0
+    SKY1 = 1
+    GREEN2 = 2
+    RED3 = 3
+    PURPLE4 = 4
+    ORANGE5 = 5
+    BLUE6 = 6
+    COLOR7 = 7
+    COLOR8 = 8
 
 
 ImageScreenshot = ImageGrab.grab()
 
-width = 18
-height = 20
 
-actual_grid = [[0] * height for i in range(width)]
+actual_grid = [[0] * height for _ in range(width)]
+exposed_grid = [[False] * height for _ in range(width)]
 
 alpha = 0.4
-leftCorner = (2591, 188)
-rightCorner = (3153, 1238)
+leftCorner = (20, 20)#(2591, 188)
+rightCorner = (550, 1080)#(3153, 1238)
 pixelJump = 5
-grayDiff = 1000
+grayDiff = 500
 ColorsDiff = 1500
 
-
+# rgb(54, 54, 54)
 def GetPixelRGB(pos):
     return ImageScreenshot.load()[pos]
 
 
-def moveMouse(x,y):
-    mouse.move(x, y, absolute=True, duration=0.0)
+def moveMouse(pos):
+
+    mouse.move(pos[0]+2578, pos[1]+174, absolute=True, duration=0.0)
 
 
-def moveMouseCell(x,y):
-    moveMouse(real_x + x * real_d, real_y + y * real_d)
+def moveMouseCell(x, y):
+    moveMouse((real_x + x * real_d, real_y + y * real_d))
 
 
 def shiftMouse(x,y):
@@ -48,9 +69,11 @@ def shiftMouse(x,y):
 
 
 def fastClick():
-    time.sleep(0.02)
+    time.sleep(0.03)
+    # time.sleep(1)
     mouse.drag(0, 0, 0, 0, absolute=False, duration=0.035)
-    time.sleep(0.02)
+    # time.sleep(1)
+    time.sleep(0.03)
 
 def longClick ():
     # todo:
@@ -76,6 +99,7 @@ def SearchForcolor(x, y, color, radius, colorDist):
     return False
 
 
+'''
 def SearchForNumbers(x, y, radius, colorDist):
     for i in range(int(x - radius * alpha), int(x + radius * alpha)):
         for j in range(int(y - radius * alpha), int(y + radius * alpha)):
@@ -83,17 +107,26 @@ def SearchForNumbers(x, y, radius, colorDist):
                 if ColorDist(GetPixelRGB((i, j)), numColor) < colorDist:
                     return i, j
     return None, None
+'''
 
 
 def FindTopLeftCorner():
+    # todo:
+    #  ugly!!!
+    allx = []
+    ally = []
     for i in range(leftCorner[1], rightCorner[1]):
         for j in range(leftCorner[0], rightCorner[0]):
             if ColorDist(GetPixelRGB((j, i)), GetPixelRGB((j + pixelJump, i))) > grayDiff:
-                return j + pixelJump, i
-    return None
+                allx.append(j+pixelJump)
+                ally.append(i)
+                # return j + pixelJump, i
+    return min(allx), min(ally)
 
 
 def FindButtomRightCorner():
+    # todo:
+    #  fix it, it might not work
     for i in range(rightCorner[1], leftCorner[1], -1):
         for j in range(rightCorner[0], leftCorner[0], -1):
             if ColorDist(GetPixelRGB((j, i)), GetPixelRGB((j - pixelJump, i))) > grayDiff:
@@ -107,54 +140,102 @@ def printGrid():
 
 
 def inputCell(x, y):
-    # todo:
-    #  use the x_cell and y_cell!!
     x_cell = real_x + x * real_d
     y_cell = real_y + y * real_d
-    if SearchForcolor(x_cell, y_cell, SKY1, alpha, ColorsDiff):
-        return Tiles.SKY1.valuse
-    elif SearchForcolor(x_cell, y_cell, GREEN2, alpha, ColorsDiff):
-        return Tiles.GREEN2.valuse
-    elif SearchForcolor(x_cell, y_cell, RED3, alpha, ColorsDiff):
-        return Tiles.RED3.valuse
-    elif SearchForcolor(x_cell, y_cell, PURPLE4, alpha, ColorsDiff):
-        return Tiles.PURPLE4.valuse
-    elif SearchForcolor(x_cell, y_cell, ORANGE5, alpha, ColorsDiff):
-        return Tiles.ORANGE5.valuse
-    elif SearchForcolor(x_cell, y_cell, BLUE6, alpha, ColorsDiff):
-        return Tiles.BLUE6.valuse
-    elif SearchForcolor(x_cell, y_cell, color7, alpha, ColorsDiff):
-        return Tiles.color7.valuse
-    elif SearchForcolor(x_cell, y_cell, FLAG, alpha, ColorsDiff):
-        return Tiles.FLAG.valuse
-    elif SearchForcolor(x_cell, y_cell, EMPTY, alpha, ColorsDiff):
-        return Tiles.EMPTY.valuse
+    isBackgroundGray = SearchForcolor(x_cell, y_cell, gray0, real_d, ColorsDiff)
+
+    if SearchForcolor(x_cell, y_cell, flag, real_d, ColorsDiff):
+        return Tiles.FLAG.value
+    elif SearchForcolor(x_cell, y_cell, sky1, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.SKY1.value
+    elif SearchForcolor(x_cell, y_cell, green2, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.GREEN2.value
+    elif SearchForcolor(x_cell, y_cell, red3, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.RED3.value
+    elif SearchForcolor(x_cell, y_cell, purple4, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.PURPLE4.value
+    elif SearchForcolor(x_cell, y_cell, orange5, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.ORANGE5.value
+    elif SearchForcolor(x_cell, y_cell, blue6, real_d, ColorsDiff) and isBackgroundGray:
+        return Tiles.BLUE6.value
+    # elif SearchForcolor(x_cell, y_cell, color7, real_d, ColorsDiff) and isBackgroundGray:
+    #     return Tiles.COLOR7.value
+    # elif SearchForcolor(x_cell, y_cell, color8, real_d, ColorsDiff) and isBackgroundGray:
+    #     return Tiles.COLOR8.value
+    elif SearchForcolor(x_cell, y_cell, emtpy, real_d, ColorsDiff):
+        return Tiles.EMPTY.value
+    elif isBackgroundGray:
+        return Tiles.GRAY0.value
 
 
 def updateGrid():
+    moveMouse(leftCorner)
+    time.sleep(0.1)
     global ImageScreenshot
     ImageScreenshot = ImageGrab.grab()
     for x in range(width):
         for y in range(height):
-            actual_grid[x][y] = inputCell(x, y)
+            value = inputCell(x, y)
+            actual_grid[x][y] = value
+            if not value == 9:
+                exposed_grid[x][y] = True
 
 
 def getNeighbors(x, y):
     neighbors = []
     for i in range(x - 1, x + 2):
         for j in range(y - 1, y + 2):
-            if (not 0 <= i < width) or (not 0 <= j <= height) or ((x, y) == (i, j)):
+            if (not 0 <= i < width) or (not 0 <= j < height) or ((x, y) == (i, j)):
                 continue
             neighbors.append(actual_grid[i][j])
     return neighbors
 
 
+def seenEnoughFlags(x, y):
+    neighbors = getNeighbors(x, y)
+    flagCount = neighbors.count(-1)
+    emptyCount = neighbors.count(9)
+
+    if emptyCount == 0:
+        return
+
+    if flagCount == actual_grid[x][y]:
+        print("seen enough flags", x, y)
+        moveMouseCell(x, y)
+        fastClick()
+
+
+def mustSeeFlags(x, y):
+    neighbors = getNeighbors(x, y)
+    flagCount = neighbors.count(-1)
+    emptyCount = neighbors.count(9)
+    value = actual_grid[x][y]
+    if value == flagCount + emptyCount:
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if (0 <= i < width) and (0 <= j < height):
+                    if actual_grid[i][j] == 9:
+                        print("must see flags", x, y)
+                        moveMouseCell(i, j)
+                        fastClick()
+                        actual_grid[i][j] = -1
+
+
+def smart(x, y):
+    # get a 4x4 grid
+    environment = [[9] * 4] * 4
+    for i in range(x, x + 4):
+        for j in range(y, y + 4):
+            if (i < width) and (j <= height):
+                environment[i-x][j-y] = actual_grid[i][j]
+
+
 def solveGrid():
-    for i in range(height):
-        for j in range(width):
-            pass
-
-
+    for i in range(width):
+        for j in range(height):
+            seenEnoughFlags(i, j)
+            mustSeeFlags(i, j)
+            # smart(i, j)
 
 
 # 2579, 489
@@ -173,30 +254,12 @@ def solveGrid():
 #         time.sleep(0.2)
 
 
-# Number Color
-# TODO:
-#   make enum list for the colors!!!
-emtpy = (156, 81, 8)
-flag = (250, 253, 255)
-gray0 = (76, 76, 76)
-sky1 = (8, 195, 255)
-green2 = (110, 218, 9)
-red3 = (209, 64, 64)
-purple4 = (176, 90, 224)
-orange5 = (236, 145, 11)
-blue6 = (101, 140, 235)
-# TODO:
-color7 = (101, 140, 235)
-color8 = (101, 140, 235)
 
-
-numberColors = [sky1, green2, red3]
 
 
 # find corners
 x_l, y_t = FindTopLeftCorner()
 x_r, y_b = FindButtomRightCorner()
-
 # realGame
 real_d = (y_b - y_t) / height
 # should be the same
@@ -204,15 +267,28 @@ real_d = (y_b - y_t) / height
 real_x = x_l + real_d / 2
 real_y = y_t + real_d / 2
 
+# moveMouse((x_l,y_t))
+# exit(0)
 # moveMouseCell(1,1)
 # print(GetPixelRGB(getPos()))
 
-printGrid()
+# updateGrid()
+# printGrid()
+# print(inputCell(11, 14))
+# moveMouseCell(11, 14)
+# mustSeeFlags(11,14)
+# exit(0)
+c = 200
+# moveMouseCell(8,8)
 
-exit(0)
+while c:
+    c -= 1
+    updateGrid()
+    printGrid()
+    solveGrid()
 
 
-
+'''
 
 # answer
 answer_x = 1492
@@ -220,7 +296,7 @@ answer_y = 523
 answer_d = 26.4
 
 
-'''
+
 time.sleep(1)
 for i in range(width):
     for j in range(height):
